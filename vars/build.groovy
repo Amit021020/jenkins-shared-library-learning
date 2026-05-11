1bb23e228ef6b5b3ef347bd0159bd0696546312e
Amit suyal <amitsuyal09@gmail.com>
2026-05-11 13:20:33 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
 def call(String imagename, String buildContext){
     echo 'This is Building'
-    sh "docker build -t ${projname} ${image}"        
+    sh "docker build -t ${imagename} ${buildContext}"        
     echo 'Build successfull'
 }
