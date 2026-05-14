e51292384864348b2b1f2068645ef5a4078f8588
Amit suyal <amitsuyal09@gmail.com>
2026-05-14 18:43:39 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,14 +1,22 @@
 def call() {
-    sh """
-        set -e
 
-        echo "Stopping old containers..."
-        docker compose down -v
+    withCredentials([
+        string(credentialsId: 'jwt-secret', variable: 'JWT_SECRET'),
+        string(credentialsId: 'mongo-uri', variable: 'MONGO_URI')
+    ]) {
 
-        echo "Building and starting services..."
-        docker compose up --build -d
+        sh """
+            echo "Stopping old containers..."
+            docker compose down -v
 
-        echo "Checking running containers..."
-        docker ps
-    """
+            echo "Building image..."
+            docker compose build
+
+            echo "Starting containers with injected secrets..."
+            JWT_SECRET=${JWT_SECRET} MONGO_URI=${MONGO_URI} docker compose up -d
+
+            echo "Checking running containers..."
+            docker ps
+        """
+    }
 }
