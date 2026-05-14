3d76536f66ddb1dbab503e8f2c13b2b744e9f4bf
Amit suyal <amitsuyal09@gmail.com>
2026-05-14 16:44:27 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,21 @@
-def call(String imagename, String buildContext){
-    echo 'This is Building'
-    sh "docker build -t ${imagename} ${buildContext}"        
-    echo 'Build successfull'
+def call(String projName, String imageTag){
+
+  withCredentials([usernamePassword(
+    credentialsId: 'dockerHubCred',
+    passwordVariable: 'dockerHubPass',
+    usernameVariable: 'dockerHubUser'
+  )]){
+
+    sh """
+      echo "${dockerHubPass}" | docker login -u "${dockerHubUser}" --password-stdin
+    """
+      
+    sh """
+      docker tag ${projName}:${imageTag} ${dockerHubUser}/${projName}:${imageTag}
+    """
+
+    sh """
+      docker push ${dockerHubUser}/${projName}:${imageTag}
+    """
+  }
 }
