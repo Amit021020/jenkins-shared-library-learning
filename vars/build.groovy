a3f2fe6de5e8e2e609604b2e18598abce9415ddd
Amit suyal <amitsuyal09@gmail.com>
2026-05-14 16:46:46 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,21 +1,3 @@
-def call(String projName, String imageTag){
-
-  withCredentials([usernamePassword(
-    credentialsId: 'dockerHubCred',
-    passwordVariable: 'dockerHubPass',
-    usernameVariable: 'dockerHubUser'
-  )]){
-
-    sh """
-      echo "${dockerHubPass}" | docker login -u "${dockerHubUser}" --password-stdin
-    """
-      
-    sh """
-      docker tag ${projName}:${imageTag} ${dockerHubUser}/${projName}:${imageTag}
-    """
-
-    sh """
-      docker push ${dockerHubUser}/${projName}:${imageTag}
-    """
-  }
+def call(String imageName, String imageContext){
+  sh "docker build -t ${imageName} ${imageContext}"
 }
